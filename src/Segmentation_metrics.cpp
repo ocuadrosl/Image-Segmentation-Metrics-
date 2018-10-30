c889b278a70af15262f806cc3dca367c2d183fe3
ocuadrosl <ocuadrosl@gmail.com>
2018-10-30 17:30:09 -0300

    main

--- src/Segmentation_metrics.cpp
+++ src/Segmentation_metrics.cpp
@@ -25,43 +25,20 @@ using namespace std;
 
 int main()
 {
-	SEG seg_1, seg_2;
-	seg_1.read("input/square_1.seg");
-	seg_2.read("input/square_2.seg");
+	SEG seg1, seg2;
+	seg1.read("input/square_1.seg");
+	seg2.read("input/square_2.seg");
 
-	//Metric *oce = new OCE();
 	AOM aom;
-	//Metric* arbelaez = new Arbelaez();
+	aom.setPenality(0.5);
+	Jaccard jaccard;
+	Dice dice;
 
-	//mssi->penality(0);
-
-	Test test;
-
-	//test.chrono(mssi, &seg_1, &seg_2);
-	//test.chrono(mssi, &seg_1, &seg_3);
-	//test.chrono(arbelaez, &seg_1, &seg_2);
-	//test.chrono(arbelaez, &seg_2, &seg_1);
-	//test.chrono(oce, &seg_1, &seg_2);
-
-	//test.chrono(oce, &seg_1, &seg_2);
-	//test.chrono(arbelaez, &seg_1, &seg_2);
-	//test.chrono(arbelaez, &seg_2, &seg_1);
-
-	//test.classes(mssi, "human_segmentations/35010", "human_segmentations/65019");
-	//test.classes(arbelaez, "human_segmentations/35010", "human_segmentations/65019");
-	//test.classes(oce, "human_segmentations/35010", "human_segmentations/65019");
-
-	//for (float i = 0; i <= 1.05; i = i + 0.05)
-	//{
-	//cout << "**************** " << i << endl;
-	aom.penality(0.5);
-	//test.classes(mssi, "seg/square", "seg/square");
-	//test.chrono(mssi, &seg_1, &seg_2);
-	cout << aom.error(seg_1, seg_2) << std::endl;
-
-	vector<vector<int> > reg_t;
-
-	//}
+	std::cout << aom.compute(seg1, seg1) << std::endl;
+	std::cout << jaccard.compute(seg1, seg1) << std::endl;
+	std::cout << dice.compute(seg1, seg2) << std::endl;
+	std::cout << dice.compute(seg1, seg2) / (2 - dice.compute(seg1, seg2)) << std::endl;
+	std::cout << 2 * jaccard.compute(seg1, seg2) / (jaccard.compute(seg1, seg2) + 1) << std::endl;
 
 	return 0;
 }
